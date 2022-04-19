b7ca980d8de855572894e1156db474c6e3f9dbd4
Alvis Hsu <junchoon14@gmail.com>
2022-04-19 15:32:41 +0800

    Install method-override

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const { Passport } = require('./config/passport')
+const methodOverride = require('method-override')
 const app = express()
 const port = 3000
 
@@ -21,6 +22,7 @@ app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
