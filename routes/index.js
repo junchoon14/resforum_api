6e3463247bd7607aed8f42f3a38708b87a4ef100
Alvis Hsu <junchoon14@gmail.com>
2022-04-19 14:14:50 +0800

    Add route sign in

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,29 @@ const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController.js')
 
-module.exports = app => {
-  app.get('/', (req, res) => {
+module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
+
+  app.get('/', authenticated, (req, res) => {
     res.redirect('/restaurants')
   })
-  app.get('/restaurants', restController.getRestaurants)
-  app.get('/admin', (req, res) => {
+  app.get('/restaurants', authenticated, restController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => {
     res.redirect('/admin/restaurants')
   })
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
   app.get('/signin', userController.signInPage)
