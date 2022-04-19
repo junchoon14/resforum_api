643d1a427109aec32d39b50cca726349a4f2c3df
Alvis Hsu <junchoon14@gmail.com>
2022-04-19 14:50:53 +0800

    Add admin restaurant index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,19 @@
+const { Restaurant } = require('../models')
+
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
-  }
+  getRestaurants: async (req, res) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true,
+        // include: [Category]
+      })
+      return res.render('admin/restaurants', { restaurants })
+    } catch (err) {
+      console.warn(err)
+    }
+  },
 }
 
 module.exports = adminController
