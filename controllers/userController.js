eda91988ba5d4365bf202c03a9fc7a10b70a9ad3
Alvis Hsu <junchoon14@gmail.com>
2022-04-19 12:42:44 +0800

    Confirm user password and unique email

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,15 +7,28 @@ const userController = {
     return res.render('signup')
   },
 
-  signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
+  signUp: async (req, res) => {
+    try {
+      if (req.body.passwordCheck !== req.body.password) {
+        req.flash('error_messages', '兩次密碼輸入不同！')
+        return res.redirect('/signup')
+      }
+      const checkUser = User.findOne({ where: { email: req.body.email } })
+      if (!checkUser) {
+        req.flash('error_messages', '信箱重複！')
+        return res.redirect('/signup')
+      }
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+      })
+      req.flash('success_messages', '成功註冊帳號！')
       return res.redirect('/signin')
-    })
-  }
+    } catch (err) {
+      console.warn(err)
+    }
+  },
 }
 
 module.exports = userController
