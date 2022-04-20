4fc3cf24b201ca2c40d6d1cceeb71ec35eccec12
Alvis Hsu <junchoon14@gmail.com>
2022-04-20 17:38:43 +0800

    Add env varible on app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const passport = require('./config/passport')
 const { Passport } = require('./config/passport')
 const methodOverride = require('method-override')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('.hbs', hbs.engine)
 app.set('view engine', '.hbs')
