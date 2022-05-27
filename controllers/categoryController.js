c767c82a2fc1fcd8c26d3a09faa8993f4f3fdbb1
Alvis Hsu <junchoon14@gmail.com>
2022-05-27 17:43:17 +0800

    Add admin categories edit and put feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,4 @@
-const db = require('../models')
-const { Category } = db
+const { Category } = require('../models')
 
 const categoryController = {
   getCategories: async (req, res) => {
@@ -8,11 +7,46 @@ const categoryController = {
         raw: true,
         nest: true
       })
+      if (req.params.id) {
+        const category = await Category.findByPk(req.params.id, {
+          raw: true,
+          nest: true
+        })
+        return res.render('admin/categories', { categories, category })
+      }
       return res.render('admin/categories', { categories })
     } catch (err) {
       console.warn(err)
     }
-  }
+  },
+  postCategory: async (req, res) => {
+    try {
+      if (!req.body.name) {
+        req.flash('error_message', 'name didn\'t exist')
+        req.redirect('back')
+      }
+      await Category.create({
+        name: req.body.name
+      })
+      return res.redirect('/admin/categories')
+    } catch (err) {
+      console.warn(err)
+    }
+  },
+  putCategory: async (req, res) => {
+    try {
+      if (!req.body.name) {
+        req.flash('error_message', 'name didn\'t exist')
+        req.redirect('back')
+      }
+      const category = await Category.findByPk(req.params.id)
+      await category.update(req.body)
+      return res.redirect('/admin/categories')
+    } catch (err) {
+      console.warn(err)
+    }
+  },
+
 }
 
 module.exports = categoryController
