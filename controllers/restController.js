61cdd5b40c356ced6c6998bdd698f065fc7bc98f
Alvis Hsu <junchoon14@gmail.com>
2022-05-28 19:50:41 +0800

    Add restaurants show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -20,6 +20,19 @@ const restController = {
     } catch (err) {
       console.warn(err)
     }
+  },
+  getRestaurant: async (req, res) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, {
+        raw: true,
+        nest: true,
+        include: Category
+      })
+      console.log(restaurant)
+      return res.render('restaurant', { restaurant })
+    } catch (err) {
+      console.warn(err)
+    }
 
   }
 }
