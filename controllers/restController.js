1bb38e5800e285ae719d3b0569dd8e2121c05c5e
Alvis Hsu <junchoon14@gmail.com>
2022-05-29 10:39:26 +0800

    Add pagination on restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,30 +1,44 @@
 const { Restaurant, Category } = require('../models')
+const pageLimit = 10
 
 
 const restController = {
   getRestaurants: async (req, res) => {
     try {
+      let offset = 0
       let whereQuery = {}
       let categoryId = ''
+      if (req.query.page) {
+        offset = (req.query.page - 1) * pageLimit
+      }
       if (req.query.categoryId) {
         categoryId = Number(req.query.categoryId)
         whereQuery['CategoryId'] = categoryId
       }
-      const restaurants = await Restaurant.findAll({
+      const restaurants = await Restaurant.findAndCountAll({
         raw: true,
         nest: true,
         include: [Category],
-        where: whereQuery
+        where: whereQuery,
+        limit: pageLimit,
+        offset: offset
       })
+      // console.log(restaurants)
       const categories = await Category.findAll({ raw: true })
-
-      const data = restaurants.map(r => ({
+      const data = restaurants.rows.map(r => ({
         ...r,
         description: r.description.substring(0, 50),
         categoryName: r.Category.name
       }))
+
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(restaurants.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1) // 產生長度符合的陣列，做出頁數陣列，[1,2,3,...]
+      let prev = page - 1 < 0 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+
       // console.log(data)
-      return res.render('restaurants', { restaurants: data, categories, categoryId })
+      return res.render('restaurants', { restaurants: data, categories, categoryId, page, pages, totalPage, prev, next })
     } catch (err) {
       console.warn(err)
     }
